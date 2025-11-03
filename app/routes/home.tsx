84b4d1f71106af522a8e3afd3a37476560dbedb1
trace_test <c1053007884@163.com>
2025-11-03 22:54:19 +0800

    CN.2666728556082859:7e795076e654fcbc28721b305029bb63_6908b53f36fea6c9d1af86bd.6908b66636fea6c9d1af86db.6908b666112de82914f2f00d:Trae CN.T(2025/11/3 22:04:22)

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,17 +1,17 @@
 import type { Route } from "./+types/home";
-import { SimpleGeoMap } from '../components/SimpleGeoMap';
+import Terrain3D from '../components/Terrain3D';
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "GeoJSON等高线图" },
-    { name: "description", content: "使用d3生成的GeoJSON等高线图可视化" },
+    { title: "3D地形可视化" },
+    { name: "description", content: "使用Three.js生成的3D地形可视化" },
   ];
 }
 
 export default function Home() {
   return (
-    <div className="container mx-auto p-4">
-      <SimpleGeoMap />
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <Terrain3D />
     </div>
   );
 }
